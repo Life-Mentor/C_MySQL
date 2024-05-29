7b46f6ec30b6356a50e25c13d8339213515be533
duck <shangaoxiangnb@gmail.com>
2024-05-30 01:30:58 +0800

    add codes

---
+++ src/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  printf("hello world\n");
+
+  return 0;
+}
+
+
